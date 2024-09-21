4ee314278f2d057fcf9361f3857adfff1a0ac972
sinxeleza <5671957@mail.ru>
2024-09-21 12:21:52 +0300

    add models

---
+++ src/models/User.model.ts
@@ -0,0 +1,27 @@
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
+
+
+export interface UserModel extends Base {}
+export default class UserModel extends TimeStamps {
+
+  @prop({type: String, unique: true, required: true})
+  email: string
+
+  @prop({type: String, unique: true, required: true})
+  username: string;
+
+  @prop({type: String, required: true})
+  passwordHash: string;
+
+  @prop({type: String})
+  name: string;
+
+  @prop({type: String})
+  surname: string;
+
+  @prop({type: String})
+  lastname: string;
+
+
+}
